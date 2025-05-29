7029813885da151fd3520ac280d1272f1f624217
FMizuno-Una <felipe@unacapital.com.br>
2025-05-29 16:20:31 -0300

    Resolucao exercicios de tupla

--- aula-03.py
+++ aula-03.py
@@ -23,3 +23,29 @@ lista = ['Mike','','Emma','Kelly','','Brad']
 while '' in lista:
     lista.remove('')
 print(lista)
+
+
+## Tuplas
+#1. Crie uma tupla com os números de 1 a 5.
+numeros = (1,2,3,4,5)
+
+#2. Imprima o segundo elemento da tupla.
+print(numeros[1])
+
+#3. Imprima o tamanho da tupla.
+print(len(numeros))
+
+#4. Concatene duas tuplas `(1, 2, 3)` `(4, 5, 6)`. Imprima a tupla concatenada.
+tupla1 = (1,2,3)
+tupla2 = (4,5,6)
+
+tupla3 = tupla1 + tupla2
+print(tupla3)
+
+#5. Crie uma tupla `(5, 10, 15)`, converta a tupla para lista, adicione o número `40` e imprima a tupla resultante.
+tupla = (5,10,15)
+lista = list(tupla)
+
+lista.append(40)
+tupla = tuple(lista)
+print(tupla)
